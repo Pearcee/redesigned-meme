1056b69cdd7f6dfd8ff024ef3778570cf44234fa
steve <sjpearce@gmail.com>
2021-04-14 08:21:27 +0100

    PWA site 3

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function myFunction() {
 }
 
 if('serviceWorker' in navigator){
-  navigator.serviceWorker.register('/sw.js')
+  navigator.serviceWorker.register('sw.js')
     .then(reg => console.log('service worker registered'))
     .catch(err => console.log('service worker not registered', err));
 }
