5352c1ea56285140ca92ceed47112e3eef9289db
steve <sjpearce@gmail.com>
2021-04-15 10:13:52 +0100

    Ad json

--- js/app.js
+++ js/app.js
@@ -12,3 +12,15 @@ if('serviceWorker' in navigator){
     .then(reg => console.log('service worker registered'))
     .catch(err => console.log('service worker not registered', err));
 }
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.name +  "<br>";
+    for (x in myObj) {
+      document.getElementById("demo").innerHTML += myObj[x] + "<br>";
+    }
+  }
+};
+
